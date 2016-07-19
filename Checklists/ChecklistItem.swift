6fb02cac9bc6c3084b5974e093efc37db07c0760
Luis De Castro <luisdecastro510@gmail.com>
2016-07-19 11:04:23 -0700

    add basic checklist logic to check and uncheck a row

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,16 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Luis De Castro on 7/18/16.
+//  Copyright © 2016 Lu. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+    var text = ""
+    var checked = false
+    
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    
+    var items: [ChecklistItem]  
+    
+    required init?(coder aDecoder: NSCoder) {
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Brush my teeth"
+        row1item.checked = true
+        items.append(row1item)
+        
+        let row2item = ChecklistItem()
+        row2item.text = "Learn iOS development"
+        row2item.checked = true
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Soccer practice"
+        row3item.checked = false
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Eat ice cream"
+        row4item.checked = true
+        items.append(row4item)
+        
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +54,46 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        
+        let item = items[indexPath.row]
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+        
+        configureCheckmarkForCell(cell, indexPath: indexPath)
+        
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            
+            let item = items[indexPath.row]
+            item.checked = !item.checked
+            
+            configureCheckmarkForCell(cell, indexPath: indexPath)
+            
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+        
+        let item = items[indexPath.row]
+        
+        if item.checked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
     }
 }
 
