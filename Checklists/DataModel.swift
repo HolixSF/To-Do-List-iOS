0620673570ea30c715b4cb773b1d8ba50524d9de
Luis De Castro <luisdecastro510@gmail.com>
2016-07-24 15:33:57 -0700

    add function to handle first time use

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -23,12 +23,26 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     func registerDefaults() {
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = ["ChecklistIndex": -1,
+                          "FirstTime": true]
         
         NSUserDefaults.standardUserDefaults().registerDefaults(dictionary)
+    }
+    
+    func handleFirstTime() {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let firstTime = userDefaults.boolForKey("FirstTime")
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            indexOfSelectedChecklist = 0
+            userDefaults.setBool(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     //    Get data from document file ---------->
     
